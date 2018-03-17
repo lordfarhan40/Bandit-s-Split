65cc2fa2fc333b6f6f1ae2256480af5de04b6f46
Mohammed Farhan <lordfarhan40@gmail.com>
2018-03-17 08:37:23 +0530

    Map and player done

---
+++ Assets/Scripts/PlayerMain.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMain : MonoBehaviour {
+
+
+    int health;
+    // Use this for initialization
+    Animator anim;
+	void Start () {
+        anim = GetComponent<Animator>();
+        health = 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("hELLO");
+    }
+
+    void damage()
+    {
+        health -= 10;
+        if(health==0)
+        {
+            anim.SetTrigger("isDead");
+        }
+    }
+}
---
+++ Assets/Scripts/ZombieControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieControl : MonoBehaviour {
+
+    public GameObject go;
+    NavMeshAgent agent;
+    Animator anim;
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(Vector3.Distance(transform.position,go.transform.position)<3)
+        {
+            anim.SetBool("isNear", true);
+            agent.Stop();
+        }else
+        {
+            anim.SetBool("isNear", false);
+            agent.Resume();
+        }
+        agent.SetDestination(go.transform.position);
+	}
+}
